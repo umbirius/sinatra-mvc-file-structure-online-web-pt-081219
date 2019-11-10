9b6cfd393a06a70ef63afebf2be4f793d0214fa8
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-10 22:45:19 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,14 @@
+class Dog > ActiveRecord::Base 
+  
+  @@all - []
+  def initialize(:name, :breed, :age)
+    self.name = :name 
+    self.breed = :breed
+    self.age = :age 
+    self << @all 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+end 
